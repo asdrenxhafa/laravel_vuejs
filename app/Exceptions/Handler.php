b8ecaa5eb27a313f3dffec51cc8d8094521fcbc6
Asdren Xhafa <ax43619@ubt-uni.net>
2020-09-25 16:55:59 +0200

    Error Handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +51,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
+
+        if ($exception instanceof NotFoundHttpException) {
+
+                return response()->view('errors.403',compact('exception'));
+
+        }
+
         return parent::render($request, $exception);
     }
 }
